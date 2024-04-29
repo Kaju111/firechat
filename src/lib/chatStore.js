73301c3d802d8aa4b5451a80b54f1c9ecc1ee7fa
kaju111 <saikiakaju275@gmail.com>
2024-04-29 20:33:51 +0530

    first commit

---
+++ src/lib/chatStore.js
@@ -0,0 +1,35 @@
+import { doc, getDoc } from "firebase/firestore";
+import { create } from "zustand";
+import { db } from "./firebase";
+import { useUserStore } from "./userStore";
+
+export const useChatStore = create((set) => ({
+  chatId: null,
+  user: null,
+  isCurrentUserBlocked: false,
+  isReceiverBlocked: false,
+  changeChat: (chatId, user) => {
+    const currentUser = useUserStore.getState().currentUser;
+
+    // CHECK IF CURRENT USER IS BLOCKED
+
+    if (user.blocked.includes(currentUser.id)) {
+      return set({
+        chatId,
+        user: null,
+        isCurrentUserBlocked: true,
+        isReceiverBlocked: false,
+      });
+    }
+    // CHECK IF RECEIVER IS BLOCKED
+
+    if (user.blocked.includes(currentUser.id)) {
+      return set({
+        chatId,
+        user: null,
+        isCurrentUserBlocked: true,
+        isReceiverBlocked: false,
+      });
+    }
+  },
+}));
